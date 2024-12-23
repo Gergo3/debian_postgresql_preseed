e9f78a187b070af3e0a3fb9a854758f41dd2256a
Simon Gergely <simon.gergely2006@gmail.com>
2024-12-23 19:30:57 +0100

    v12.8

--- install.d/psql.sh
+++ install.d/psql.sh
@@ -3,11 +3,20 @@
 
 #potgresql
 echo 1 > /target/psql_test
-#sed -i "s/#port = 5432					# (change requires restart)/port = 31454/" /target/etc/postgresql/15/main/postgresql.conf
-echo "port = 31454" >> /target/etc/postgresql/15/main/postgresql.conf
-echo "host    all             all             0.0.0.0/0               md5" >> /target/etc/postgresql/15/main/pg_hba.conf
+sed -i "s/^port =.*/port = 31454/" /target/etc/postgresql/15/main/postgresql.conf
+sed -i "s/^#listen_addresses.*/listen_addresses = '*'/" /target/etc/postgresql/15/main/postgresql.conf
+#echo "port = 31454" >> /target/etc/postgresql/15/main/postgresql.conf
+echo "host    all             all             0.0.0.0/0               scram-sha-256" >> /target/etc/postgresql/15/main/pg_hba.conf
 #chroot /target
-in-target sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'md5fe2b307508b3c01f8b1825f10bb6dbce';"
+#in-target sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'md5fe2b307508b3c01f8b1825f10bb6dbce';"
 #in-target sudo -u postgres psql -c "CREATE USER myuser WITH PASSWORD 'mypassword';"
 #in-target sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE postgres TO myuser;"
 #exit
+
+in-target sudo -u postgres psql -c "CREATE DATABASE stx1"
+in-target sudo -u postgres psql -c "CREATE USER proba"
+in-target sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE stx1 TO proba"
+
+#sudo -u postgres psql -c "CREATE DATABASE stx1"
+#sudo -u postgres psql -c "CREATE USER proba WITH PASSWORD 'valami'"
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE stx1 TO proba"
